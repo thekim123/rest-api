6549f8e1f839ed3c9147d2ae7af097173db7a196
thekim123 <tacdo12@naver.com>
2024-11-08 16:48:33 +0900

    Body for BadRequest

--- src/test/java/me/thekim123/restapi/event/EventControllerTests.java
+++ src/test/java/me/thekim123/restapi/event/EventControllerTests.java
@@ -127,6 +127,10 @@ public class EventControllerTests {
         this.mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(this.objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists())
+        ;
     }
 }
